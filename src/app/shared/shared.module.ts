179a85c667a3f6e42240ca4eb48e2951ad3bb3db
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-02-14 18:15:28 +0200

    clean code

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderTopComponent } from './header-top/header-top.component';
+
+
+
+@NgModule({
+  declarations: [HeaderTopComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
