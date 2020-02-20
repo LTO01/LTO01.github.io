91efcc82d558aaae2dd578ec88f7ef8f9b3e9446
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-02-21 01:55:47 +0200

    add routing to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SideNavModule } from './views/side-nav.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,40 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { SharedModule } from './shared/shared.module';
-import { AboutComponent } from './views/about/about.component';
-import { GamesComponent } from './views/games/games.component';
-import { ExperienceComponent } from './views/experience/experience.component';
-import { PortfolioComponent } from './views/portfolio/portfolio.component';
-import { SkillsComponent } from './views/skills/skills.component';
-import { ContactsComponent } from './views/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    GamesComponent,
-    ExperienceComponent,
-    PortfolioComponent,
-    SkillsComponent,
-    ContactsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    SharedModule
+      SideNavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/side-nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { shareReplay, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
+})
+export class SideNavComponent implements OnInit {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+  }
+
+}
