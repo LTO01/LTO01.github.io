7bb987f6bae2afa473a9029c4b2693ef4494221f
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-02-20 15:16:04 +0200

    fix errors

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,14 @@
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedMaterialModule } from './shared-material.module';
 
 
-
 @NgModule({
-  declarations: [MainNavComponent],
+  declarations: [],
   imports: [
     CommonModule,
     SharedMaterialModule
   ],
-  exports: [MainNavComponent]
+  exports: []
 })
 export class SharedModule { }
