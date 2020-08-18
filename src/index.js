9520ca3486c6e2f507397154584db59a4f024cb7
L. Theodore Obonye <ltobonye@gmail.com>
2020-08-18 13:58:48 +0200

    add tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-document.body.innerHTML = "Hello World!"
+import { formatDistanceToNow } from 'date-fns';
+
+const date = '1996-09-13 10:00:00';
+document.body.textContent = formatDistanceToNow(new Date(date)) + ' ago';
